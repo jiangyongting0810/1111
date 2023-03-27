5be1654a300a46b97a2b5e7eef355f8ce8123cb0
Jiang YongTing <jiangyongting0810@gmail.com>
2023-03-27 23:18:27 +0800

    使用插槽改四个页面

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,22 +1,24 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss'
+import s from './WelcomeLayout.module.scss'
 import chart from '../../assets/icons/chart.svg'
 import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
 
 export const Forth = defineComponent({
   setup: (props, context) => {
-     return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img src={chart} />
-          <h2>每日提醒<br/>不遗漏每一笔账单</h2>
-        </div>
-        <div class={s.actions}>
-          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-          <RouterLink to="/start" >完成</RouterLink>
-          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-        </div>
-    </div>
-     )
-  }
+    return () => (
+       <WelcomeLayout>
+         {{
+           icon:<img src={chart} />,
+           title:<h2>每日提醒<br/>不遗漏每一笔账单</h2>,
+           buttons:  
+           <>
+           <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+           <RouterLink to="/start" >完成</RouterLink>
+           <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+           </>
+         }}
+       </WelcomeLayout>
+    )
+  }  
 })
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,22 +1,25 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss'
+import s from './WelcomeLayout.module.scss'
 import cloud from '../../assets/icons/cloud.svg'
 import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
 
 export const Third = defineComponent({
   setup: (props, context) => {
-     return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img src={cloud} />
-          <h2>每日提醒<br/>不遗漏每一笔账单</h2>
-        </div>
-        <div class={s.actions}>
-          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-          <RouterLink to="/welcome/4" >下一页</RouterLink>
-          <RouterLink to="/start" >跳过</RouterLink>
-        </div>
-    </div>
-     )
-  }
+    return () => (
+       <WelcomeLayout>
+         {{
+           icon:<img src={cloud} />,
+           title:<h2>每日提醒<br/>不遗漏每一笔账单</h2>,
+           buttons:  
+           <>
+           <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+           <RouterLink to="/welcome/4" >下一页</RouterLink>
+           <RouterLink to="/start" >跳过</RouterLink>
+           </>
+         }}
+       </WelcomeLayout>
+    )
+  }  
 })
+
