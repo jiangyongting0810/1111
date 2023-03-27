3a3ada776f19f75ccd7ecd5244b7274e4cb2a67d
Jiang YongTing <jiangyongting0810@gmail.com>
2023-03-27 22:43:02 +0800

    四个欢迎页面

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -8,8 +8,8 @@ export const First = defineComponent({
      return () => (
       <div class={s.wrapper}>
         <div class={s.card}>
-          <img class={s.pig} src={pig} />
-          <h2>会挣钱<br/>还要会省钱</h2>
+          <img  src={pig} />
+          <h2>每日提醒<br/>不遗漏每一笔账单</h2>
         </div>
         <div class={s.actions}>
           <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
