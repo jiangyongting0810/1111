5bb44488e5eda55a4b233ab13affb4be4f56af91
Jiang YongTing <jiangyongting0810@gmail.com>
2023-04-02 13:07:16 +0800

    添加icon组件

---
+++ src/shared/FloatButton.tsx
@@ -0,0 +1,11 @@
+import { defineComponent } from 'vue';
+import { Icon } from './Icon';
+export const FloatButton = defineComponent({
+  setup: (props, context) => {
+     return () => (
+       <div>
+        <Icon name='add'/>
+       </div>
+     )
+  }
+})
