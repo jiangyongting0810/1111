894ff56f3d5865fcf4f016d6b6e0a4abd6f3a16f
Jiang YongTing <jiangyongting0810@gmail.com>
2023-04-08 16:38:30 +0800

    完成数字输入功能

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -12,25 +12,49 @@ export const InputPad = defineComponent({
   setup: (props, context) => {
     const now = new Date()
     const refDate = ref<Date>(now)
+    const appendText = (n:number | string) => {
+      const nString = n.toString()
+      const dotIndex = refAmount.value.indexOf('.')
+      if(refAmount.value.length >= 13){
+        return
+      }
+      if(dotIndex >= 0 && refAmount.value.length - dotIndex >2){
+        return
+      }
+      if(nString === '.'){
+        if(dotIndex >= 0){//已经有小数点
+          return
+        }
+      }else if(nString === '0'){
+        // if(dotIndex === -1){//没有小数点
+          if(refAmount.value === "0"){
+            return
+          // }
+        }
+      }else{
+        if(refAmount.value === '0'){
+          refAmount.value=''
+        }
+      }
+      refAmount.value += n.toString()
+    }
     const buttons = [
-      { text: '1', onClick: () => { } },
-      { text: '2', onClick: () => { } },
-      { text: '3', onClick: () => { } },
-      { text: '清空', onClick: () => { } },
-      { text: '4', onClick: () => { } },
-      { text: '5', onClick: () => { } },
-      { text: '6', onClick: () => { } },
-      { text: '+', onClick: () => { } },
-      { text: '7', onClick: () => { } },
-      { text: '8', onClick: () => { } },
-      { text: '9', onClick: () => { } },
-      { text: '-', onClick: () => { } },
-      { text: '.', onClick: () => { } },
-      { text: '0', onClick: () => { } },
-      { text: '删', onClick: () => { } },
+      {text: '1', onClick: () => { appendText(1) }},
+      { text: '2', onClick: () => { appendText(2) } },
+      { text: '3', onClick: () => { appendText(3) } },
+      { text: '4', onClick: () => { appendText(4) } },
+      { text: '5', onClick: () => { appendText(5) } },
+      { text: '6', onClick: () => { appendText(6) } },
+      { text: '7', onClick: () => { appendText(7) } },
+      { text: '8', onClick: () => { appendText(8) } },
+      { text: '9', onClick: () => { appendText(9) } },
+      { text: '.', onClick: () => { appendText('.') } },
+      { text: '0', onClick: () => { appendText(0) } },
+      { text: '清空', onClick: () => {refAmount.value=''} },
       { text: '提交', onClick: () => { } },
     ]
     const refShowPop = ref(false)
+    const refAmount = ref('')
     return () => <>
       <div class={s.dateAndAmount}>
         <span class={s.date}>
@@ -43,7 +67,7 @@ export const InputPad = defineComponent({
             </Popup>
           </span>
         </span>
-        <span class={s.amount}>199.12</span>
+        <span class={s.amount}>{refAmount.value}</span>
       </div>
       <div class={s.buttons}>
         {buttons.map(button =>
