0a84a97ff162e23615bb4c7bba2c83005deb4e31
Jiang YongTing <jiangyongting0810@gmail.com>
2023-04-08 11:46:10 +0800

    修复日期选择bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import styleImport, { VantResolve } from 'vite-plugin-style-import';
+// @ts-nocheck
 import { svgstore } from './src/vite_plugins/svgstore';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+    vue(),
     vueJsx({
-      transformOn:true,
-      mergeProps:true
-      // options are passed on to @vue/babel-plugin-jsx
+      transformOn: true,
+      mergeProps: true
     }),
     svgstore(),
+    styleImport({
+      resolves: [VantResolve()],
+    }),
   ]
 })
