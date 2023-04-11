31aa633d61fce8e7884b1eaf27fc8efdbd200dd5
Jiang YongTing <jiangyongting0810@gmail.com>
2023-04-11 22:34:07 +0800

    完成创建标签表单功能

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -3,6 +3,7 @@ import { MainLayout } from '../../layouts/MainLayout';
 import { Button } from '../../shared/Button';
 import { EmojiSelect } from '../../shared/EmojiSelect';
 import { Icon } from '../../shared/Icon';
+import { Rules, validata } from '../../shared/validata';
 import s from './TagCreate.module.scss';
 export const TagCreate = defineComponent({
   props: {
@@ -15,14 +16,20 @@ export const TagCreate = defineComponent({
       name:'',
       sign:''
     })
+    const errors = reactive<{[k in keyof typeof formDate]?:string[]}>({})
     const onSubmit = (e:Event) =>{
-      console.log(toRaw(formDate));
-      // const rules =[
-      //   {key:'name',required:true,message:'必填'},
-      //   {key:'name',pattern:/^.{0,4}$/,message:'只能填1到4个字符'},
-      //   {key:'sign',required:true}
-      // ]
-      // const errors = validate(formDate,rules)
+      const rules:Rules<typeof formDate> =[
+        {key:'name',type:"required",message:'必填'},
+        {key:'name',type:"pattern",regex:/^.{0,4}$/,message:'只能填1到4个字符'},
+        {key:'sign',type:"required",message:'必填'}
+      ]
+      Object.assign(errors,{
+        name:undefined,
+        sign:undefined
+      })
+      Object.assign(errors,validata(formDate,rules))
+      console.log(errors);
+      
       e.preventDefault()
     }
     return () => (
@@ -38,7 +45,7 @@ export const TagCreate = defineComponent({
                   <input v-model={formDate.name} class={[s.formItem, s.input, s.error]}></input>
                 </div>
                 <div class={s.formItem_errorHint}>
-                  {/* <span>{errors['name'][0]}</span> */}
+                  <span>{errors['name'] ? errors['name'][0]:'　'}</span>
                 </div>
               </label>
             </div>
@@ -49,7 +56,7 @@ export const TagCreate = defineComponent({
                   <EmojiSelect v-model={formDate.sign} class={[s.formItem, s.emojiList, s.error]}/>
                 </div>
                 <div class={s.formItem_errorHint}>
-                  <span>必填</span>
+                  <span>{errors['sign'] ? errors['sign'][0]:'　'}</span>
                 </div>
               </label>
             </div>
