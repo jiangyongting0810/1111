ccc5fc4fa99182a8a2fe4763a790990f385cd110
Jiang YongTing <jiangyongting0810@gmail.com>
2023-04-15 10:35:07 +0800

    实现 ItemList 页面样式

--- src/shared/time.tsx
+++ src/shared/time.tsx
@@ -1,9 +1,9 @@
-export class Time{
-  date:Date;
-  constructor(date = new Date()){
-    this.date = date
+export class Time {
+  date: Date;
+  constructor(date = new Date()) {
+    this.date = date;
   }
-  format(pattern = 'YYYY-MM-DD'){
+  format(pattern = 'YYYY-MM-DD') {
     // 目前支持的格式有 YYYY MM DD HH mm ss SSS
     const year = this.date.getFullYear()
     const month = this.date.getMonth() + 1
@@ -31,7 +31,8 @@ export class Time{
   }
   lastDayOfYear() {
     return new Time(new Date(this.date.getFullYear() + 1, 0, 0, 0, 0, 0));
-  }getRaw() {
+  }
+  getRaw() {
     return this.date
   }
   add(amount: number, unit: 'year' | 'month' | 'day' | 'hour' | 'minute' | 'second' | 'millisecond') {
@@ -68,30 +69,5 @@ export class Time{
     }
     return new Time(date)
   }
-}
 
-// export const time = (date = new Date()) => {
-//   const api = {
-//     format: (pattern = 'YYYY-MM-DD')=>{
-//       // 目前支持的格式有 YYYY MM DD HH mm ss SSS
-//       const year = date.getFullYear()
-//       const month = date.getMonth() + 1
-//       const day = date.getDate()
-//       const hour = date.getHours()
-//       const minute = date.getMinutes()
-//       const second = date.getSeconds()
-//       const msecond = date.getMilliseconds()
-//       return pattern.replace(/YYYY/g, year.toString())
-//         .replace(/MM/, month.toString().padStart(2, '0'))
-//         .replace(/DD/, day.toString().padStart(2, '0'))
-//         .replace(/HH/, hour.toString().padStart(2, '0'))
-//         .replace(/mm/, minute.toString().padStart(2, '0'))
-//         .replace(/ss/, second.toString().padStart(2, '0'))
-//         .replace(/SSS/, msecond.toString().padStart(3, '0'))
-//     },
-//     firstDayOfMonth:()=>{
-//       return time (new Date(date.getFullYear(),date.getMonth(),1,0,0,0))
-//     }
-//   }
-//   return api
-// }
+}
