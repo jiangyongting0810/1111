40a849d84df7636ea4139d865a865c22a8b03253
Jiang YongTing <jiangyongting0810@gmail.com>
2023-04-16 10:27:32 +0800

    添加Button组件事件传递

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,8 +1,6 @@
 import { defineComponent, PropType } from 'vue';
 import s from './Button.module.scss'
 
-
-
 export const Button = defineComponent({
   props:{
     onClick:{
@@ -18,7 +16,7 @@ export const Button = defineComponent({
   },
   setup: (props, context) => {
      return () => (
-       <button type={props.type} class={[s.button,s[props.level]]}>
+       <button type={props.type} class={[s.button,s[props.level]]} onClick={props.onClick}>
           {context.slots.default?.()}
        </button>
      )
