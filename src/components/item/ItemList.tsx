e984a5c10d9add281250a1380ca877098c104684
Jiang YongTing <jiangyongting0810@gmail.com>
2023-04-16 10:27:55 +0800

    修复自定义时间相关bug

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -41,6 +41,11 @@ export const ItemList = defineComponent({
       e.preventDefault()
       refOverlayVisible.value = false
     }
+    const onSelect = (value:string)=>{
+      if(value === '自定义时间'){
+        refOverlayVisible.value = true
+      }
+    }
     return () => (
       <MainLayout>
         {
@@ -48,7 +53,10 @@ export const ItemList = defineComponent({
             title:()=>"山竹记账",
             icon:()=><Icon name='menu' />,
             default:()=><>
-            <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
+            <Tabs classPrefix={'customTabs'} 
+            v-model:selected={refSelected.value}
+            onUpdate:selected={onSelect}
+            >
               <Tab name="本月">
                 <ItemSummary
                   startDate={timeList[0].start.format()}
@@ -81,8 +89,8 @@ export const ItemList = defineComponent({
                     <FormItem label='结束时间' v-model={customTime.end} type='date' />
                     <FormItem >
                       <div class={s.actions}>
-                      <Button type='button' >取消</Button>
-                      <Button type='submit' >确认</Button>
+                        <Button type='button' onClick={()=>refOverlayVisible.value = false}>取消</Button>
+                        <Button type='submit' >确认</Button>
                       </div>
                     </FormItem>
                   </Form>
