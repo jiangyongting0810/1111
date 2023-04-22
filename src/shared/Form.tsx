f84b6feaba6a4d119b6a174f85674f911cca5b2c
Jiang YongTing <jiangyongting0810@gmail.com>
2023-04-22 21:32:02 +0800

    发送验证码成功之后触发倒计时

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -34,11 +34,28 @@ export const FormItem = defineComponent({
     },
     onClick: Function as PropType<()=>void>,
     placeholder:String,
-    options:Array as PropType<Array<{value:string,text:string}>>
+    options:Array as PropType<Array<{value:string,text:string}>>,
+    countFrom:{
+      type:Number,
+      default:60
+    }
   },
   emits:['update:modelValue'],
   setup: (props, context) => {
     const refDateVisible =ref(false)
+    const timer = ref<number>()
+    const count = ref<number>(props.countFrom)
+    const isCounting = computed(()=>!!timer.value)
+    const startCount = () =>
+    timer.value = setInterval(() => {
+      count.value -= 1
+      if (count.value === 0) {
+        clearInterval(timer.value)
+        timer.value = undefined
+        count.value = props.countFrom
+      }
+    }, 1000)
+  context.expose({ startCount })
     const content = computed(()=>{
       switch(props.type){
         case'text':
@@ -58,7 +75,9 @@ export const FormItem = defineComponent({
             <input onInput={(e:any)=>context.emit('update:modelValue',e.target.value)}
             class={[s.formItem,s.input,s.validationCodeInput]}  
             placeholder={props.placeholder}/>
-            <Button onClick={props.onClick} class={s.validationCodeButton}>发送验证码</Button>
+            <Button disabled={isCounting.value} onClick={props.onClick} class={s.validationCodeButton}>
+              {isCounting.value ? count.value:"发送验证码"}
+            </Button>
           </>
         case'select':
           return<select class={[s.formItem,s.select]} value={props.modelValue} onChange={(e:any)=>{context.emit('update:modelValue',e.target.value);
--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { defineComponent, PropType, reactive } from 'vue';
+import { defineComponent, PropType, reactive, ref } from 'vue';
 import { MainLayout } from '../layouts/MainLayout';
 import { Button } from '../shared/Button';
 import { Form, FormItem } from '../shared/Form';
@@ -7,72 +7,65 @@ import { Icon } from '../shared/Icon';
 import { validate } from '../shared/validate';
 import s from './SignInPage.module.scss';
 export const SignInPage = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
   setup: (props, context) => {
     const formData = reactive({
-      email:'',
-      code:''
+      email: '',
+      code: ''
     })
     const errors = reactive({
-      email:[],
-      code:[]
+      email: [],
+      code: []
     })
-    const onClickSendValidationCode = async ()=>{
-      console.log('11111')
-      const response = await axios.post('/api/v1/validation_codes', { email: formData.email })      
-      console.log(response);
-    }
-    const onSubmit = (e:Event)=>{
-      // console.log('submit')
+    const refValidationCode = ref<any>()
+    const onSubmit = (e: Event) => {
       e.preventDefault()
-      Object.assign(errors,{
-        email:[],code:[]
+      Object.assign(errors, {
+        email: [], code: []
       })
-      Object.assign(errors,validate(formData,[
+      Object.assign(errors, validate(formData, [
         { key: 'email', type: 'required', message: '必填' },
         { key: 'email', type: 'pattern', regex: /.+@.+/, message: '必须是邮箱地址' },
         { key: 'code', type: 'required', message: '必填' },
       ]))
-      // console.log(formData)
-      // console.log(errors)
+    }
+    const onClickSendValidationCode = async () => {
+      const response = await axios.post('/api/v1/validation_codes', { email: formData.email })
+        .catch(()=>{
+          //失败
+        })
+      // 成功
+      refValidationCode.value.startCount()
     }
     return () => (
       <MainLayout>{
         {
-          title:()=>'登录',
-          icon:()=><Icon name='back'/>,
-          default:()=>(
-          <div class={s.wrapper}>
-            <div class={s.logo}>
-              <Icon name='logo' class={s.icon}/>
-              <h1 class={s.appName}>山竹记账</h1>
+          title: () => '登录',
+          icon: () => <Icon name="back" />,
+          default: () => (
+            <div class={s.wrapper}>
+              <div class={s.logo}>
+                <Icon class={s.icon} name="logo" />
+                <h1 class={s.appName}>山竹记账</h1>
+              </div>
+              <Form onSubmit={onSubmit}>
+                <FormItem label="邮箱地址" type="text"
+                  placeholder='请输入邮箱，然后点击发送验证码'
+                  v-model={formData.email} error={errors.email?.[0]} />
+                <FormItem ref={refValidationCode} 
+                  label="验证码" 
+                  type="validationCode"
+                  placeholder='请输入六位数字'
+                  countFrom={60}
+                  onClick={onClickSendValidationCode}
+                  v-model={formData.code} error={errors.code?.[0]} />
+                <FormItem style={{ paddingTop: '96px' }}>
+                  <Button>登录</Button>
+                </FormItem>
+              </Form>
             </div>
-            <Form onSubmit={onSubmit}>
-              <FormItem 
-                label='邮箱地址' 
-                placeholder='请输入邮箱然后点记发送验证码' 
-                type='text' 
-                v-model={formData.email} 
-                error={errors.email?.[0]}/>
-              <FormItem label='验证码' 
-                placeholder='请输入六位验证码'  
-                type='validationCode' 
-                onClick={onClickSendValidationCode}
-                v-model={formData.code} 
-                error={errors.code?.[0]}/>
-              <FormItem style={{paddingTop:"96px"}}>
-                <Button>登录</Button>
-              </FormItem>
-            </Form>
-          </div>
           )
         }
-      }
-      </MainLayout>
+      }</MainLayout>
     )
   }
 })
