65958a99f6cb61982f106edd9ac490db0a6d8acd
Jiang YongTing <jiangyongting0810@gmail.com>
2023-04-28 21:01:20 +0800

    优化了，但是代码行数变多了

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -17,49 +17,6 @@ import { StartPage } from "../views/StartPage";
 import { Welcome } from "../views/Welcome";
 import { SignInPage } from "../views/SignInPage";
 import { StatisticsPage } from "../views/StatisticsPage";
-import { http } from "../shared/Http";
-
-
-// export const routes: RouteRecordRaw[] = [
-//   { path: '/', redirect: '/welcome' },
-//   {
-//     path: '/welcome',
-//     component: Welcome,
-//     beforeEnter: (to, from, next) => {
-//       localStorage.getItem('skipFeatures') === 'yes' ? next('/start') : next()
-//     },
-//     children: [
-//       { path: '', redirect: '/welcome/1' },
-//       { path: '1', name: "Welcome1", components: { main: First, footer: FirstActions }, },
-//       { path: '2', name: "Welcome2", components: { main: Second, footer: SecondActions }, },
-//       { path: '3', name: "Welcome3", components: { main: Third, footer: ThirdActions }, },
-//       { path: '4', name: "Welcome4", components: { main: Forth, footer: ForthActions }, },
-//     ]
-//   },
-//   { path: '/start', component: StartPage },
-//   {
-//     path: '/items', component: ItemPage,
-//     children: [
-//       { path: '', component: ItemList },
-//       { path: 'create', component: ItemCreate },
-//     ]
-//   },
-//   {
-//     path: '/tags', component: TagPage,
-//     children: [
-//       { path: 'create', component: TagCreate },
-//       { path: ':id/edit', component: TagEdit }
-//     ]
-//   },
-//   {
-//     path: '/sign_in', component: SignInPage
-//   },
-//   {
-//     path: '/statistics', component: StatisticsPage
-//   }
-// ]
-
-
 
   export const routes:RouteRecordRaw[] = [
     { path: '/', redirect: '/welcome' },
@@ -78,12 +35,6 @@ import { http } from "../shared/Http";
       ]
     },
     {path:'/start',component:StartPage},
-    // {path:'/items',component:ItemPage,
-    //   children:[
-    //     {path:'',component:ItemList},
-    //     {path:'create',component:ItemCreate}
-    //   ]
-    // },
     {
       path: '/items', component: ItemPage,
       children: [
