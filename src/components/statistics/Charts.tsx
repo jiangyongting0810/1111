7d5c9e6a8a921f0a3d3fe99cc42640afc89ecc5a
Jiang YongTing <jiangyongting0810@gmail.com>
2023-05-07 17:53:28 +0800

    初步转换折线图数据

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,9 +1,14 @@
-import { defineComponent, PropType, ref } from 'vue';
+import { computed, defineComponent, onMounted, PropType, ref } from 'vue';
 import { FormItem } from '../../shared/Form';
 import s from './Charts.module.scss';
 import { LineChart } from './LineChart';
 import { PieChart } from './PieChart';
 import { Bars } from './Bars';
+import { http} from '../../shared/Http';
+
+type Data1Item = {happen_at:string,amount:number}
+type Data1 = Data1Item[]
+
 export const Charts = defineComponent({
   props: {
     startDate: {
@@ -17,13 +22,28 @@ export const Charts = defineComponent({
   },
   setup: (props, context) => {
     const category = ref('expenses')
+    //方便知道类型，并且赋值为空数
+    const data1 = ref<Data1>([])
+    //转换成需要的数据格式
+    const betterData1 = computed(()=>
+      data1.value.map(item=>[item.happen_at,item.amount] as [string,number])
+    )
+
+    onMounted(async()=>{
+      const response = await http.get<{groups:Data1,summary:number}>('/items/summary',{
+        _mock:'itemSummary'
+      })
+      console.log('response.data')
+      console.log(response.data)
+      data1.value = response.data.groups
+    })
     return () => (
       <div class={s.wrapper}>
         <FormItem label='类型' type='select' options={[
           {value:'expenses',text:'支出'},
           {value:'income',text:'收入'}
         ]} v-model={category.value}/>
-        <LineChart/>
+        <LineChart data={betterData1.value}/>
         <PieChart/>
         <Bars/>
       </div>
