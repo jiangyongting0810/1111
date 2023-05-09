42fd1e70fa27c2fdd7671dd93d4e81d033a0a332
Jiang YongTing <jiangyongting0810@gmail.com>
2023-05-09 21:14:12 +0800

    完成 bar chart

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -63,6 +63,13 @@ export const Charts = defineComponent({
       }))
     )
 
+    const betterData3 = computed<{tag:Tag,amount:number,percent:number}[]>(()=>{
+      const total = data2.value.reduce((sum, item) => sum + item.amount, 0)
+      return data2.value.map(item => ({
+        ...item,
+        percent: Math.round(item.amount / total * 100) 
+      }))
+    })
     onMounted(async()=>{
       const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
         happen_after: props.startDate,
@@ -82,7 +89,7 @@ export const Charts = defineComponent({
         ]} v-model={kind.value}/>
         <LineChart data={betterData1.value}/>
         <PieChart data={betterData2.value}/>
-        <Bars/>
+        <Bars data={betterData3.value}/>
       </div>
     )
   }
