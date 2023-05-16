20751e6d8ff6e7573808eb5b70dd0e94ea098090
Jiang YongTing <jiangyongting0810@gmail.com>
2023-05-16 22:19:23 +0800

    使用 store.$reset() 代替 reset

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -1,15 +1,16 @@
-import { defineComponent, onMounted, PropType, reactive, ref, watch } from 'vue';
-import { RouterLink } from 'vue-router';
-import { useAfterMe } from '../../hooks/useAfterMe';
-import { Button } from '../../shared/Button';
-import { Center } from '../../shared/Center';
-import { Datetime } from '../../shared/Datetime';
-import { FloatButton } from '../../shared/FloatButton';
-import { http } from '../../shared/Http';
-import { Icon } from '../../shared/Icon';
-import { Money } from '../../shared/Money';
-import { useItemStore } from '../../stores/useItemStore';
-import s from './ItemSummary.module.scss';
+import { defineComponent, onMounted, onUnmounted, PropType, reactive, ref, watch } from 'vue'
+import { RouterLink } from 'vue-router'
+import { useAfterMe } from '../../hooks/useAfterMe'
+import { Button } from '../../shared/Button'
+import { Center } from '../../shared/Center'
+import { Datetime } from '../../shared/Datetime'
+import { FloatButton } from '../../shared/FloatButton'
+import { http } from '../../shared/Http'
+import { Icon } from '../../shared/Icon'
+import { Money } from '../../shared/Money'
+import { useItemStore } from '../../stores/useItemStore'
+import { useMeStore } from '../../stores/useMeStore'
+import s from './ItemSummary.module.scss'
 export const ItemSummary = defineComponent({
   props: {
     startDate: {
@@ -22,97 +23,115 @@ export const ItemSummary = defineComponent({
     }
   },
   setup: (props, context) => {
-    if(!props.startDate || !props.endDate){
-      return ()=>(
-        <div>请先选择时间范围</div>
-    )}
-    const itemStore = useItemStore(['items',props.startDate,props.endDate])
+    if (!props.startDate || !props.endDate) {
+      return () => <div>请先选择时间范围</div>
+    }
+    const itemStore = useItemStore(['items', props.startDate, props.endDate])
+    useAfterMe(() => itemStore.fetchItems(props.startDate, props.endDate))
 
-    // onMounted(fetchItems)
-    useAfterMe(()=>itemStore.fetchItems(props.startDate,props.endDate))
+    watch(
+      () => [props.startDate, props.endDate],
+      () => {
+        itemStore.$reset()
+        itemStore.fetchItems()
+      }
+    )
 
     const itemsBalance = reactive({
-      expenses: 0, income: 0, balance: 0
+      expenses: 0,
+      income: 0,
+      balance: 0
     })
-
-    watch(()=>[props.startDate,props.endDate],()=>{
-      console.log("观察两个数据变化");
-      itemStore.reset()
-      itemStore.fetchItems()
-    })
-    
-    const fetchItemsBalance = async()=>{
-      if(!props.startDate || !props.endDate){ return }
-      const response = await http.get('/items/balance',{
-        happen_after: props.startDate,
-        happen_before: props.endDate,
-      },{_mock: 'itemIndexBalance',})
-      Object.assign(itemsBalance,response.data)
+    const fetchItemsBalance = async () => {
+      if (!props.startDate || !props.endDate) {
+        return
+      }
+      const response = await http.get(
+        '/items/balance',
+        {
+          happen_after: props.startDate,
+          happen_before: props.endDate
+        },
+        {
+          _mock: 'itemIndexBalance'
+        }
+      )
+      Object.assign(itemsBalance, response.data)
     }
-    // onMounted(fetchItemsBalance)
     useAfterMe(fetchItemsBalance)
-    watch(()=>[props.startDate,props.endDate],()=>{
-      Object.assign(itemsBalance,{
-        expenses: 0, income: 0, balance: 0
-      })
-      fetchItemsBalance()
-    })
+    watch(
+      () => [props.startDate, props.endDate],
+      () => {
+        Object.assign(itemsBalance, {
+          expenses: 0,
+          income: 0,
+          balance: 0
+        })
+        fetchItemsBalance()
+      }
+    )
     return () => (
       <div class={s.wrapper}>
-      {(itemStore.items && itemStore.items.length > 0)? (
-        <>
-          <ul class={s.total}>
-            <li>
-              <span>收入</span>
-              <Money value={itemsBalance.income} />
-            </li>
-            <li>
-              <span>支出</span>
-              <Money value={itemsBalance.expenses} />
-            </li>
-            <li>
-              <span>净收入</span>
-              <Money value={itemsBalance.balance} />
-            </li>
-          </ul>
-          <ol class={s.list}>
-            {itemStore.items.map((item) => (
+        {itemStore.items && itemStore.items.length > 0 ? (
+          <>
+            <ul class={s.total}>
               <li>
-                <div class={s.sign}>
-                  <span>{item.tags && item.tags.length >0 ? item.tags[0].sign : '💰'}</span>
-                </div>
-                <div class={s.text}>
-                  <div class={s.tagAndAmount}>
-                    <span class={s.tag}>{item.tags && item.tags.length >0 ? item.tags[0].name : '未分类'}</span>
-                    <span class={s.amount}>￥<Money value={item.amount}/></span>
-                  </div>
-                  <div class={s.time}><Datetime value={item.happen_at}/></div>
-                </div>
+                <span>收入</span>
+                <Money value={itemsBalance.income} />
               </li>
-            ))}
-          </ol>
-          <div class={s.more}>
-            {itemStore.hasMore ?
-              <Button onClick={()=>itemStore.fetchItems(props.startDate,props.endDate)}>加载更多</Button> :
-              <span>没有更多</span>
-            }
-          </div>
-        </>
-      ) : (<>
-        <Center class={s.pig_wrapper}>
-                <Icon name="pig" class={s.pig} />
-              </Center>
-              <div class={s.button_wrapper}>
-                <RouterLink to='/items/create'>
-                  <Button class={s.button}>开始记账</Button>
-                </RouterLink>
-              </div>
+              <li>
+                <span>支出</span>
+                <Money value={itemsBalance.expenses} />
+              </li>
+              <li>
+                <span>净收入</span>
+                <Money value={itemsBalance.balance} />
+              </li>
+            </ul>
+            <ol class={s.list}>
+              {itemStore.items.map((item) => (
+                <li>
+                  <div class={s.sign}>
+                    <span>{item.tags && item.tags.length > 0 ? item.tags[0].sign : '💰'}</span>
+                  </div>
+                  <div class={s.text}>
+                    <div class={s.tagAndAmount}>
+                      <span class={s.tag}>{item.tags && item.tags.length > 0 ? item.tags[0].name : '未分类'}</span>
+                      <span class={s.amount}>
+                        ￥<Money value={item.amount} />
+                      </span>
+                    </div>
+                    <div class={s.time}>
+                      <Datetime value={item.happen_at} />
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ol>
+            <div class={s.more}>
+              {itemStore.hasMore ? (
+                <Button onClick={() => itemStore.fetchNextPage(props.startDate, props.endDate)}>加载更多</Button>
+              ) : (
+                <span>没有更多</span>
+              )}
+            </div>
           </>
-      )}
-      <RouterLink to='/items/create'>
-        <FloatButton iconName='add'/>
-      </RouterLink>
-    </div>
+        ) : (
+          <>
+            <Center class={s.pig_wrapper}>
+              <Icon name="pig" class={s.pig} />
+            </Center>
+            <div class={s.button_wrapper}>
+              <RouterLink to="/items/create">
+                <Button class={s.button}>开始记账</Button>
+              </RouterLink>
+            </div>
+          </>
+        )}
+        <RouterLink to="/items/create">
+          <FloatButton iconName="add" />
+        </RouterLink>
+      </div>
     )
   }
 })
